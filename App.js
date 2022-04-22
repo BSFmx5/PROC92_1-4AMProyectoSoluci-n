6140c45378a66951946393860ae550c1c25b859c
BFSmx5 <97541881+BSFmx5@users.noreply.github.com>
2022-04-22 17:55:44 -0500

    Update App.js

--- App.js
+++ App.js
@@ -16,10 +16,10 @@ function App() {
       <Stack.Navigator initialRouteName="Inicio" screenOptions={{
         headerShown: false
       }}>
-        <Stack.Screen name="Inicio" component={HomeScreen} />
-        <Stack.Screen name="Mapa estelar" component={StartMapScreen} />
-        <Stack.Screen name="Foto diaria" component={DailyPicScreen} />
-        <Stack.Screen name="Nave espacial" component={SpaceCraftScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="StarMap" component={StartMapScreen} />
+        <Stack.Screen name="DailyPic" component={DailyPicScreen} />
+        <Stack.Screen name="SpaceCraft" component={SpaceCraftScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
